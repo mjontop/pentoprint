e95581d6a41419fb2be3cc4f218be8b81993efe6
mjontop <mjontop0602@gmail.com>
2020-03-15 01:07:07 +0530

    bannner images added

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,18 @@ var name = '/src/img/book'
 i = 1
 
 function slidebanner() {
-    i = i + 1
-    if (i > 4) {
-        i = 1
-    }
-    var newname = name + i + '.png'
-    $("#banner").attr("src", newname)
+    $("#banner").fadeToggle(300, function() {
+        $(".banner > div > div").css("background-color", "red")
+        $(".imgposition"+i).css("background-color", "black")
+        i = i + 1
+        if (i > 4) {
+            i = 1
+        }
+        var newname = name + i + '.png'
+        $("#banner").attr("src", newname)
+    })
+    $("#banner").fadeToggle(100)
+
 }
+
+setInterval(slidebanner, 2000)
