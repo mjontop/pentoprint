0af99d8888750b0ecf5312840ba7d94a99047397
Manikant Jha <mjontop0602@gmail.com>
2020-06-25 16:34:55 +0530

    Added Images

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,18 @@
-var name = '/src/img/book'
-i = 1
+var name = '/src/img/book';
+i = 1;
 
 function slidebanner() {
-    $("#banner").fadeToggle(300, function () {
-        $(".banner > div > div").css("background-color", "red")
-        $(".imgposition" + i).css("background-color", "black")
-        i = i + 1
-        if (i > 4) {
-            i = 1
-        }
-        var newname = name + i + '.png'
-        $("#banner").attr("src", newname)
-    })
-    $("#banner").fadeToggle(100)
-
+	$('#banner').fadeToggle(300, function () {
+		$('.banner > div > div').css('background-color', 'red');
+		$('.imgposition' + i).css('background-color', 'black');
+		i = i + 1;
+		if (i > 3) {
+			i = 1;
+		}
+		var newname = name + i + '.jpg';
+		$('#banner').attr('src', newname);
+	});
+	$('#banner').fadeToggle(100);
 }
 
-setInterval(slidebanner, 3000)
+setInterval(slidebanner, 3000);
